e1035b4825d402c27f95a68ed77eb6b156da3025
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-08-19 14:17:44 -0500

    exercises complete

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,11 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student annalyssa = new Student("Annalyssa");
+            Console.WriteLine("The Student class works! " + annalyssa.StudentId + " is a student!");
+
+            Student teehoos = new Student("Teehoos");
+            Console.WriteLine("The Student class works! " + teehoos.StudentId + " is a student!");
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the Main method in Program
@@ -7,9 +8,87 @@ namespace SchoolPractice
 {
     public class Student
     {
-        private string Name;
-        private int StudentId;
-        private int NumberOfCredits;
-        private double Gpa;
+        private static int nextStudentId = 1;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; } = 0;
+        public double Gpa { get; set; } = 0.0;
+
+        public Student(string name, int studentId, int numberOfCredits, double gpa)
+        {
+            Name = name;
+            StudentId = studentId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
+
+        public Student(string name, int studentId) : this(name, studentId, 0, 0) { }
+
+        public Student(string name) : this(name, nextStudentId)
+        {
+            nextStudentId++;
+        }
+
+        public string StudentInfo()
+        {
+            return (Name + " has a GPA of: " + Gpa);
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            double totalQualityScore = Gpa * NumberOfCredits;
+            totalQualityScore += courseCredits * grade;
+            NumberOfCredits += courseCredits;
+            Gpa = totalQualityScore / courseCredits;
+        }
+
+        public string GetGradeLevel(int numOfCredits)
+        {
+            if (numOfCredits < 30)
+            {
+                return "freshman";
+            }
+
+            if (numOfCredits < 60)
+            {
+                return "sophomore";
+            }
+
+            if (numOfCredits < 90)
+            {
+                return "junior";
+            }
+
+            if (numOfCredits >= 90)
+            {
+                return "senior";
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name + " (Credits: " + NumberOfCredits + ", GPA: " + Gpa + ")";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Student studentObj = obj as Student;
+            return StudentId == studentObj.StudentId;
+        }
     }
 }
